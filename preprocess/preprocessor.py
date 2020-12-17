fd225f51b59319a34cbc6ac7b15dccaf88daa65b
Junrong Lau <junr0ng@hotmail.com>
2020-12-18 00:33:01 +0800

    Added imports and updated preprocessing methods

--- preprocess/preprocessor.py
+++ preprocess/preprocessor.py
@@ -1,10 +1,24 @@
+from utils.reformat import reformat_reviews_df
+from utils.reformat import reformat_profiles_df
+from utils.clean import clean_text
+
+
 class Preprocessor:
-    def __init__(self, reviews_df, profiles_df):
+    def __init__(self, config, reviews_df, profiles_df):
+        self.config = config
         self.reviews_df = reviews_df
         self.profiles_df = profiles_df
 
-    def keep_english(self):
-        self.reviews_df = self.reviews_df[self.reviews_df['language'] == 'English']
+    def preprocess_reviews(self):
+        self.reviews_df = reformat_reviews_df(self.reviews_df)
+        self.reviews_df['cleaned_text'] = clean_text(self.reviews_df['decoded_comment'],
+                                                     self.config.preprocessing.contractions_path,
+                                                     self.config.preprocessing.slangs_path)
+        return self.reviews_df
 
-    def preprocess_text(self):
-        pass
+    def preprocess_profiles(self):
+        self.profiles_df = reformat_profiles_df(self.profiles_df)
+        return self.profiles_df
+
+
+
